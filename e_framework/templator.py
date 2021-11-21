cc36d46210b3714220e4932039ff144602133a8e
nat0t <nataniel1987@gmail.com>
2021-11-21 09:00:37 +0300

    Added base template.

--- e_framework/templator.py
+++ e_framework/templator.py
@@ -1,5 +1,4 @@
-from jinja2 import Template
-import os
+from jinja2 import Environment, FileSystemLoader
 
 
 def render(template_name, folder='templates', **kwargs):
@@ -9,7 +8,8 @@ def render(template_name, folder='templates', **kwargs):
     :param kwargs: параметры
     :return:
     """
-    file_path = os.path.join(folder, template_name)
-    with open(file_path, encoding='utf-8') as file:
-        template = Template(file.read())
+
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
     return template.render(**kwargs)
